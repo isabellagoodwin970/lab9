b5ed211035f60e71b2c97afdd32a92b36159914b
isabellagoodwin970 <bellagoodwin@protonmail.com>
2024-04-11 23:19:47 -0400

    main

--- encoder_decoder.py
+++ encoder_decoder.py
@@ -1,8 +1,11 @@
+#Isabella Goodwin
 def encoder(password):
     result =""
     for i in range(len(password)):
         digit = int(password[i])
         digit +=3 # Must include an if statement checking for new digit greater than 10.  Password cannot be greater than 8 digits.
+        if digit >9:
+             digit -= 10
         result += str(digit)
     return result
 
@@ -18,3 +21,26 @@ def decoder(password):
         result += str(digit) # Adds decoded digit to resultant string.
     
     return result
+
+def main():
+    flag = True
+    while flag:
+        print("Password Encoder ")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Exit")
+        option = int(input("Enter option number: "))
+        if option == 3:
+            flag = False
+            print("Thanks for using Password Encoder!")
+            break
+        elif option == 1:
+            passcode = input("Enter password to be encoded(8 digits): ")
+            passcode = encoder(passcode)
+            print("Password encoded!")
+        elif option == 2:
+            passcode = input("enter password to be decoded(8 digits): ")
+            passcode = decoder(passcode)
+            print("Password decoded!")
+        print()
+main()
